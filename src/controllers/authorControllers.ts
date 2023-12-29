b183e2c2976a9e9f3d37d3ee3fa0bc6cf227960a
chahat sagar <chahatsagar2003@gmail.com>
2023-12-29 17:11:17 +0530

    CRUD OPERATIONS FOR AUTHOR DONE

--- src/controllers/authorControllers.ts
+++ src/controllers/authorControllers.ts
@@ -12,3 +12,101 @@ export async function getAllAuthors() : Promise<Author[] | null>{
         return null;
     }
 }
+
+export async function getAuthorById(id : number) : Promise<Author | null>{
+    try{
+        const user = await prisma.author.findUnique({
+            where : {
+                id : id
+            }
+        });
+
+        if (user) return user;
+
+        return null;
+
+    }
+    catch(error : any){
+        console.log(error);
+        return null;
+    }
+}
+
+export async function createAuthor(data : any) : Promise<Author | null>{
+    
+    try{
+        const found = await prisma.author.findFirst({
+            where : {
+                firstName : data.firstName,
+                lastName : data.lastName 
+            },
+        });
+
+        if(found) return found;
+        const user = await prisma.author.create({
+            data : {
+                firstName : data.firstName,
+                lastName : data.lastName
+            }
+        });
+
+        return user;
+    
+    }
+    catch(error : any){
+        console.log(error);
+        return null;
+    }
+}
+
+export async function updateAuthorById(id: number, data: any): Promise<Author | null> {
+    try {
+      const found = await prisma.author.findUnique({
+        where: {
+          id: id,
+        },
+      });
+  
+      if (!found) {
+        const user = await prisma.author.create({
+          data: {
+            firstName: data.firstName,
+            lastName: data.lastName,
+          },
+        });
+  
+        return user;
+      }
+  
+      const user = await prisma.author.update({
+        where: {
+          id: id,
+        },
+        data: {
+          firstName: data.firstName,
+          lastName: data.lastName,
+        },
+      });
+  
+      return user;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+}
+
+export async function deleteAuthorById(id : number) : Promise<Author | null>{
+    try{
+        const user = await prisma.author.delete({
+            where : {
+                id : id
+            }
+        });
+
+        return user;
+    }
+    catch(error : any){
+        console.log(error);
+        return null;
+    }
+}
